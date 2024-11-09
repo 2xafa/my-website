577dd481c2becb697510da9765bbd2b2db68ec2b
2xafa <safademirdag@yahoo.com.tr>
2024-11-09 20:41:47 +0300

    .

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    i18n: {
+      // These are all the locales you want to support in
+      // your application
+      locales: ['en', 'tr'],
+      // This is the default locale you want to be used when visiting
+      // a non-locale prefixed path e.g. `/hello`
+      defaultLocale: 'tr',
+      // This is a list of locale domains and the default locale they
+      localeDetection: false,
+    },
+  }
